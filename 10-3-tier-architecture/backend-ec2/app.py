76e24d3fc37d1e91f19ff8a3b508ba35af5ba58b
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2025-05-02 11:55:49 +0530

    Update app.py

--- 10-3-tier-architecture/backend-ec2/app.py
+++ 10-3-tier-architecture/backend-ec2/app.py
@@ -1,1 +1,22 @@
+from flask import Flask
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    db = mysql.connector.connect(
+        host=os.environ['DB_HOST'],
+        user=os.environ['DB_USER'],
+        passwd=os.environ['DB_PASSWORD'],
+        database=os.environ['DB_NAME']
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT NOW();")
+    result = cursor.fetchone()
+    return f"Hello from backend! Current DB time: {result[0]}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
 
